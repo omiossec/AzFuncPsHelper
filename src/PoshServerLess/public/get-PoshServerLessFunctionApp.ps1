5cdae062101fc773996d21ac1bb203b6d2346efb
olivier.miossec <olivier.miossec@metsys.fr>
2019-09-17 08:22:35 +0200

    Doc

--- src/PoshServerLess/public/get-PoshServerLessFunctionApp.ps1
+++ src/PoshServerLess/public/get-PoshServerLessFunctionApp.ps1
@@ -1,6 +1,28 @@
 function get-PoshServerlessFunctionApp
 {
 
+        <#
+    .SYNOPSIS
+    
+    Create an Azure Function App object by reading a folder
+    
+    .DESCRIPTION
+    
+    Create an Azure Function App object by reading a folder
+    If you have download a functions App or create it you can use this function to create an AzFunctionsApp object from the folder
+    It will read the host.json and function folder
+    
+    .PARAMETER FunctionAppPath
+    [String] the function App Path
+    if the path doesn't exist the cmdlet will fail
+
+    .PARAMETER FunctionAppName
+    [String] the function App Name
+    
+
+           
+    #>
+
     [OutputType([AzFunctionsApp])]
     [CmdletBinding()]
     param(
